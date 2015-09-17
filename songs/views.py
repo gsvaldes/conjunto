6237cff2f63cf5b280b92dbae9c2382a7bbd5f69
gsvaldes <valdesgeoffrey@gmail.com>
2015-09-17 15:33:28 -0500

    initial dynamic templates

--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Song
 
 def song_list(request):
-	return render(request, 'songs/song_list.html', {})
+	songs = Song.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'songs/song_list.html', {'songs': songs})
