8002cb24ef8235232c59e89c2c51502775fad78e
gsvaldes <valdesgeoffrey@gmail.com>
2015-09-17 16:39:44 -0500

    started urls for post_detail

--- songs/urls.py
+++ songs/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.song_list, name="song_list")
+    url(r'^$', views.song_list, name="song_list"),
+    url(r'^song/(?P<pk>[0-9]+)/$', views.song_detail, name='song_detail'),
 ]
