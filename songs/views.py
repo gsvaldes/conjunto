625ae6ac5ecbea06f48a7ab4e9b7735a25062084
gsvaldes <valdesgeoffrey@gmail.com>
2015-09-24 13:32:58 -0500

    added view and template for song post

--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Song
 
 def song_list(request):
 	songs = Song.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'songs/song_list.html', {'songs': songs})
+
+def song_detail(request, pk):
+	song = get_object_or_404(Song, pk=pk)
+	return render(request, 'songs/song_detail.html', {'song': song})
